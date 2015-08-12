2a82ffab71c11919df960deab2d9106783b97ed7
Tim White & Joe Hartman <student@epicodus.com>
2015-08-12 15:40:39 -0700

    Add one-player game.

--- app/app.php
+++ app/app.php
@@ -6,13 +6,23 @@
     $app->register(new Silex\Provider\TwigServiceProvider(), array('twig.path' => __DIR__.'/../views'));
 
     $app->get('/', function() use ($app) {
-     return $app['twig']->render('rps_form.html.twig');
+        return $app['twig']->render('rps_form.html.twig');
     });
 
     $app->get('/results', function() use ($app) {
         $new = new RockPaperScissors;
         $output = $new->rockPaperScissorsMethod($_GET['player1'], $_GET['player2']);
         return $app['twig']->render('rps_results.html.twig', array('results' => $output));
+    });
+
+    $app->get('/one_player', function() use ($app){
+        return $app['twig']->render('rps_form_one_player.html.twig');
+    });
+
+    $app->get('/one_player_results', function() use ($app) {
+        $new = new RockPaperScissors;
+        $output = $new->rockPaperScissorsOnePlayer($_GET['player1']);
+        return $app['twig']->render('rps_results_one_player.html.twig', array('results' => $output));
     });
 
     return $app;
--- src/RockPaperScissors.php
+++ src/RockPaperScissors.php
@@ -24,6 +24,55 @@ class RockPaperScissors
         }
         return $winner;
     }
+
+    function rockPaperScissorsOnePlayer($input)
+    {
+        $results_array = array();
+
+        $p1 = 'Player 1 wins';
+        $p2 = 'Computer wins';
+
+        $rps_array = array('r'=>'r','p'=>'p','s'=>'s');
+        $random_choice =  array_rand($rps_array, 1);
+
+        if ($random_choice == 'r') {
+            array_push ($results_array, 'Rock');
+        } elseif ($random_choice == 'p') {
+            array_push ($results_array, 'Paper');
+        } else {
+            array_push ($results_array, 'Scissors');
+        }
+
+
+
+        if ($input == $random_choice) {
+            $winner = 'Tie';
+        } elseif ($input == 'r' && $random_choice == 's') {
+            $winner = $p1;
+        } elseif ($input == 'r' && $random_choice == 'p') {
+            $winner = $p2;
+        } elseif ($input == 'p' && $random_choice == 'r') {
+            $winner = $p1;
+        } elseif ($input == 'p' && $random_choice == 's') {
+            $winner = $p2;
+        } elseif ($input == 's' && $random_choice == 'p') {
+            $winner = $p1;
+        } elseif ($input == 's' && $random_choice == 'r') {
+            $winner = $p2;
+        }
+
+        array_push ($results_array, $winner);
+
+        if ($input == 'r') {
+            array_push ($results_array, 'Rock');
+        } elseif ($input == 'p') {
+            array_push ($results_array, 'Paper');
+        } else {
+            array_push ($results_array, 'Scissors');
+        }
+
+        return $results_array;
+    }
 }
 
 ?>
