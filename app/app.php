7ed42c78d6887e1f176674bec16f2f1068cc0d8b
Tim White & Joe Hartman <student@epicodus.com>
2015-08-12 12:14:02 -0700

    add app.php and index.php to project

---
+++ app/app.php
@@ -0,0 +1,19 @@
+<?php
+    require_once __DIR__.'/../vendor/autoload.php';
+    require_once __DIR__.'/../src/RockPaperScissors.php';
+
+    $app = new Silex\Application();
+    $app->register(new Silex\Provider\TwigServiceProvider(), array('twig.path' => __DIR__.'/../views'));
+
+    $app->get('/', function() use ($app) {
+     return $app['twig']->render('rps_form.html.twig');
+    });
+
+    $app->get('/results', function() use ($app) {
+        $new = new RockPaperScissors;
+        $output = $new->rockPaperScissorsMethod($_GET['player1'], $_GET['player2']);
+        return $app['twig']->render('rps_results.html.twig', array('results' => $output));
+    });
+
+    return $app;
+ ?>
